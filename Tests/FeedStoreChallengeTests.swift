93ac52cb5bae8174478bd74c143628853e56cbc6
Satveer Singh <satveer.y.singh@gmail.com>
2020-06-14 13:01:08 +1000

    Delete empties previously inserted cache

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -10,6 +10,7 @@ class InMemoryFeedStore: FeedStore {
     private var cache: (feed: [LocalFeedImage], timestamp: Date)?
     
     func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        cache = nil
         completion(nil)
     }
     
@@ -93,9 +94,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	}
 
 	func test_delete_emptiesPreviouslyInsertedCache() {
-//		let sut = makeSUT()
-//
-//		assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
+		let sut = makeSUT()
+
+		assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
 	}
 
 	func test_storeSideEffects_runSerially() {
