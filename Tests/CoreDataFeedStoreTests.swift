1587cbaef43f3cdfd855853ddee2f6b0460b33ab
Satveer Singh <satveer.y.singh@gmail.com>
2020-06-14 15:38:35 +1000

    CoreDataFeedStore delete has no side effects on empty cache

--- Tests/CoreDataFeedStoreTests.swift
+++ Tests/CoreDataFeedStoreTests.swift
@@ -179,9 +179,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
 
     func test_delete_hasNoSideEffectsOnEmptyCache() {
-//        let sut = makeSUT()
-//
-//        assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
+        let sut = makeSUT()
+
+        assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
     }
 
     func test_delete_deliversNoErrorOnNonEmptyCache() {
